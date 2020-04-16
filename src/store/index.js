1ca77aad26d7ce06ab26405b605b2668fee305ba
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-04-16 15:09:46 +0100

    basic example

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,12 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vuex from "vuex";
+import Vue from "vue";
+import confirmer from "./confirmer";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state: {},
   modules: {
-  }
-})
+    confirmer,
+  },
+});
